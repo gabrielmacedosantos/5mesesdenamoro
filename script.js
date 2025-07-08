774143c393d72c265971527142618bdeec172868
Gabriel Macedo <99225975+gabrielmacedosantos@users.noreply.github.com>
2025-07-07 23:20:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     { src: 'foto4.jpeg', desc: 'Abraço que me acolhe e me faz sentir seguro! 🤗' },
     { src: 'foto6.png', desc: 'Realizou o meu sonho, tour na arena em um dia especial com uma pessoa especial! 🏟️🏆❤️' },
     { src: 'foto7.jpeg', desc: 'Primeiro rolêzinho de festa junina que eu vou! kkk 🔥🌽'},
-    { src: 'foto8.jpeg', desc: 'Simplesmente zueirinha haha, e isso te faz unica! 🤪😻' },
+    { src: 'foto8.jpeg', desc: 'Simplesmente zueirinha haha, e isso te faz única! 🤪😻' },
     { src: 'foto5.jpeg', desc: 'Hoje completamos 5 meses dessa nossa história linda que só cresce a cada dia. Amoréco, eu sou tão grato por ter vc ao meu lado, por cada sorriso, abraço e cada momento que compartilhamos juntos. Vc é meu aconchego, minha alegria, meu tudãooooo. Que venham muitos meses e anos pela frente, sempre com esse amor que só aumenta e aquece o coração e acima de tudo, que vc continue sendo essa mulher de Deus que vc é!. Te amo demais, minha pretinhaaaaa! 🫶🏼❤️' },
   ];
 
